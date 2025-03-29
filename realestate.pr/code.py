d1f6fb74ac9ecf271e029bd70cbe67d99221c59a
Sergio Albors <sergioalborsjuez@gmail.com>
2025-03-29 21:43:56 +0100

    .

--- realestate.pr/code.py
+++ realestate.pr/code.py
@@ -7,3 +7,7 @@ st.header("Choose the ticker")
 stocks = ["MU", "AAPL", "MSFT"]
 choice1 = st.selectbox("Which of the following stocks you want to predict?", stocks)
 
+days = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+choice2 = st.selectbox("select the number of days ahead (2 weeks max)", days)
+
+button2 = st.button("predict")
