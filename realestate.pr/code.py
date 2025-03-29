f53cc20d0e254d96b81a57a8b9640dcc33a3cf0f
Sergio Albors <sergioalborsjuez@gmail.com>
2025-03-29 21:30:52 +0100

    .

--- realestate.pr/code.py
+++ realestate.pr/code.py
@@ -1,4 +1,8 @@
 import streamlit as st
-st.title("yes")
+st.title("Stock prediction")
 
-st.write("This is my new app")
+st.write("This is my new app that hopefully will predict the price of a stock")
+st.header("Choose the ticker")
+choice1 = st.selectbox("Which of the following stocks you want to predict?")
+    ("MU", "MSFT", "AAPL")
+button1 = st.button("Predict")
