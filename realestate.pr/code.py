03f3eebae6a94fb8679c8011d692510453356680
Sergio Albors <sergioalborsjuez@gmail.com>
2025-03-29 21:35:31 +0100

    .

--- realestate.pr/code.py
+++ realestate.pr/code.py
@@ -5,5 +5,5 @@ st.write("This is my new app that hopefully will predict the price of a stock")
 st.header("Choose the ticker")
 
 stocks = ["MU", "AAPL", "MSFT"]
-choice1 = st.selectbox("Which of the following stocks you want to predict?"), (stocks)
+choice1 = st.selectbox("Which of the following stocks you want to predict?", stocks)
 
