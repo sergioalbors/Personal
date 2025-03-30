c4909e14c9cf01388c69b7ecf80194f157c33152
Sergio Albors <sergioalborsjuez@gmail.com>
2025-03-30 20:30:03 +0200

    .

--- learning regressions/linear/LINEAR REGRESSION EXAMPLE.py
+++ learning regressions/linear/LINEAR REGRESSION EXAMPLE.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-data = pd.read_csv("C:\\Users\\salbo\\Desktop\\Personal\\learning regressions\\regressiondata.csv")
+data = pd.read_csv("C:\\Users\\salbo\\Desktop\\Personal\\learning regressions\\linear\\regressiondata.csv")
 x = data["STUDYTIME"].astype(float)
 y = data["SCORE"].astype(float)
 
@@ -12,6 +12,9 @@ m , b = np.polyfit(x, y, 1)
 yi = m * x + b
 plt.scatter(x,y)
 plt.plot(x, yi, linestyle='-', color="red", label=f"y = {m}x + {b}")
+
+plt.text(min(x), max(y), f"m = {m:.2f}\nb = {b:.2f}", fontsize=12, color="blue")
+
 
 plt.xlabel("X")
 plt.ylabel("Y")
---
+++ testingapp/testingapp.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import pandas as pd
+
+data = pd.DataFrame({"X": [0, 0, 0, 0, 0, 0], "Y" : [0, 0, 0, 0, 0, 0]})
