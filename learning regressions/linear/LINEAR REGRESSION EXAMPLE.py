c672e485b92faca3a3edab3dcb9a8a698bcbacab
Sergio Albors <sergioalborsjuez@gmail.com>
2025-03-30 20:50:58 +0200

    .

--- learning regressions/linear/LINEAR REGRESSION EXAMPLE.py
+++ learning regressions/linear/LINEAR REGRESSION EXAMPLE.py
@@ -3,6 +3,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+horas = 2
+
 data = pd.read_csv("C:\\Users\\salbo\\Desktop\\Personal\\learning regressions\\linear\\regressiondata.csv")
 x = data["STUDYTIME"].astype(float)
 y = data["SCORE"].astype(float)
@@ -15,8 +18,10 @@ plt.plot(x, yi, linestyle='-', color="red", label=f"y = {m}x + {b}")
 
 plt.text(min(x), max(y), f"m = {m:.2f}\nb = {b:.2f}", fontsize=12, color="blue")
 
+nota = m * horas + b
 
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.title("First example of a linear regression")
 plt.show()
+print("Your grade in the exam will be aprox=", nota)
--- realestate.pr/code.py
+++ realestate.pr/code.py
@@ -3,25 +3,18 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
 
+
 st.title("Welcome to a linear regression example")
 st.write("There is an upcoming math exam next week, and you want to know the grade you will get based on the amount of hours you study  ")
 st.write("The professor said in class that this exam will be very similar in terms of difficulty so the ratio grade-study hours shouldn't change too much compared to last exam ")
 x = st.slider("how many hours have you studied?", min_value=0, max_value=10)
 st.write("Hours studied:", x)
-#regression
-data = pd.read_csv("C:\\Users\\salbo\\Desktop\\Personal\\learning regressions\\linear\\regressiondata.csv")
-x = data["STUDYTIME"].astype(float)
-y = data["SCORE"].astype(float)
+hours = x
+m = 0.89
+b = 2.30
+grade = m * hours + b
 
-m , b = np.polyfit(x, y, 1)
-
-yi = m * x + b
-plt.scatter(x,y)
-plt.plot(x, yi, linestyle='-', color="red", label=f"y = {m}x + {b}")
-
-plt.text(min(x), max(y), f"m = {m:.2f}\nb = {b:.2f}", fontsize=12, color="blue")
-
-st.write("y=")
+st.write("If you have studied", x, "hours, your grade will aproximatelty be:", grade)
 
 
 
