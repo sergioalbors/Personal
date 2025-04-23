c98c4b31e7926e3606abb8376be9609c9bd3da22
Sergio Albors <sergioalborsjuez@gmail.com>
2025-04-23 18:55:57 +0200

    .

--- learning regressions/polynomia/n-1.py
+++ learning regressions/polynomia/n-1.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-n = 4
+n = 10
 x = np.random.uniform(-100, 100, n)
-y = np.random.uniform(-100,100, n) 
+y = np.random.uniform(-100, 100, n)
 #a = np.polyfit(x, y, n-1)
 
 V = np.vander(x, N=len(x), increasing = False)
@@ -13,7 +13,7 @@ polinomio = np.poly1d(a)
 x_fit = np.linspace(min(x), max(x), 100)
 y_fit = polinomio(x_fit)
 
-plt.scatter(x, y)
+plt.scatter(x, y, color = 'red')
 plt.plot(x_fit, y_fit)
 plt.show()
 print(polinomio)
